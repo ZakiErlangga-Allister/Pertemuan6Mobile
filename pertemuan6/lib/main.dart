0279a62e192eb842e0fffd7ac4df9a95d21e3f8f
Zaki Muhammad Athallah Erlangga <zakihalilintar76@gmail.com>
2025-10-03 10:25:25 +0700

    Tugas dasar widget

---
+++ pertemuan6/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Belajar Scaffold & AppBar"),
+          backgroundColor: Colors.teal,
+          actions: const [
+            Icon(Icons.search),
+            Icon(Icons.settings),
+          ],
+        ),
+        body: const Center(
+          child: Text("Ini Body Aplikasi"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
